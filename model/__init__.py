58e9f474a6b141c8f6e0beca004b452c6418ccd0
我运气超好 <yj20220275@stud.tjut.edu.cn>
2024-06-12 22:27:31 +0800

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,13 @@
+from model.Unet.model_Unet import U_Net as Unet
+from model.ACM.model_ALCnet import ASKCResNetFPN as ALCNet
+from model.ACM.model_ACM import ASKCResUNet as ACM
+from model.DNANet.model_DNANet import DNANet as DNANet
+###wo
+###from model.ISNet.model_ISNet import ISNet as ISNet
+from model.RISTDnet.model_RISTDnet import RISTDnet as RISTDnet
+from model.UIUNet.model_UIUNet import UIUNET as UIUNet
+from model.ISTDUNet.model_ISTDUNet import ISTDU_Net as ISTDU_Net
+from model.RDIAN.model_RDIAN import RDIAN as RDIAN
+from model.SCTransNet.SCTransNet import SCTransNet as SCTransNet
+from model.SCTransNet import Config as config
+from model.MSHNet.MSHNet import MSHNet as MSHNet
